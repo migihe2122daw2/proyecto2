47a695eefab04e4c6b62934d74ae21ba518cc14c
ansasa <ansasa2122daw2@protonmail.com>
2021-12-09 17:46:14 +0100

    Commit Andrea 3 09/12

---
+++ claseLibro.php
@@ -0,0 +1,12 @@
+<?php
+
+    class Libro{
+        public $titol;
+        public $autor;
+        public $ISBN;
+        public $prestec;
+        public $iniciprestec;
+        public $codiusuari;
+
+    }
+?>
